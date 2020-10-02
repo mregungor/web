e6262ba7552e07e804e775c52baf4873435aa4ab
Emre Güngör <59082849+mregungor@users.noreply.github.com>
2020-10-02 12:43:26 +0300

    rundocsio css test

---
+++ content/prj/rastr/rastrLDA.js
@@ -0,0 +1,29 @@
+/* Implement Rasterization Line Drawing Algorithms (LDAs)*/
+
+/*
+    1- Mid-Point LDA
+    2- Bresenham LDA
+    3- DDA LDA
+*/
+
+/* Define 2D Vector */
+
+
+//1- Mid-Point LDA
+function lda_midPoint(xs, ys, xn, yn)
+{
+
+}
+
+//2- Bresenham LDA
+function lda_bresenham(xs, ys, xn, yn)
+{
+
+}
+
+//3- DDA LDA
+function lda_dda(xs, ys, xn, yn)
+{
+
+}
+
